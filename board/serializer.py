67bdf8b03f1c2191969ff145af2b2f1513b36e83
박재욱 <parkjaewook@bagjaeug-ui-Macmini.local>
2023-08-05 21:29:45 +0900

    DB 교체 SQLite -> MySQL

--- board/serializer.py
+++ board/serializer.py
@@ -5,4 +5,4 @@ class BoardSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Board
-        fields = ('id', 'user_id', 'title', 'contents', 'create_time', 'update_time')
+        fields = ('id', 'user', 'title', 'contents', 'create_time', 'update_time')
--- config/settings.py
+++ config/settings.py
@@ -87,12 +87,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+import my_settings as my_settings
+
+DATABASES = my_settings.DATABASES
 
 
 # Password validation
